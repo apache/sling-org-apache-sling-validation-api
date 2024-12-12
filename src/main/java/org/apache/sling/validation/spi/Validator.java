ec16f20a7da7e9032ebe67e4eb8291690c643b31
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-12-12 14:48:59 +0100

    SLING-12553 fix javadoc error

--- src/main/java/org/apache/sling/validation/spi/Validator.java
+++ src/main/java/org/apache/sling/validation/spi/Validator.java
@@ -36,7 +36,8 @@ public interface Validator<T> {
      *
      * The validator can enforce the type of the given data just by setting the appropriate parameter type {@code T} which can be any non-primitive class.
      * Depending on whether this type is an array or not the {@code validate} method is called differently:
-     * <table summary="">
+     * <table>
+     *  <caption></caption>
      *  <tr>
      *    <th>T is array type</th>
      *    <th>Valuemap contains array value</th>
